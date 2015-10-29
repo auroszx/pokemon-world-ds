3bb8eb50effd1dc6c5016f5407018ad44020ec71
auroszx <auros.andres@gmail.com>
2015-10-29 13:14:57 -0430

    Fixed variable names

--- movement.lua
+++ movement.lua
@@ -7,28 +7,28 @@ playerPosY = 48
 
 function playerMovement()
 	if Keys.held.Up or Keys.held.Down or Keys.held.Left or Keys.held.Right then
-		Player:playAnimation(SCREEN_DOWN, x, y, direction)
+		Player:playAnimation(SCREEN_DOWN, playerPosX, playerPosY, playerDirection)
 	else                                                                                
-		Player:drawFrame(SCREEN_DOWN, x, y, -2 + direction * 3)
+		Player:drawFrame(SCREEN_DOWN, playerPosX, playerPosY, -2 + playerDirection * 3)
 	end
 	
 	if Keys.held.Down then
-		direction = 1
-		y = y + 1
+		playerDirection = 1
+		playerPosY = playerPosY + 1
 	end
 	
 	if Keys.held.Left then
-		direction = 2
-		x = x - 1
+		playerDirection = 2
+		playerPosX = playerPosX - 1
 	end
 	
 	if Keys.held.Right then
-		direction = 3
-		x = x + 1
+		playerDirection = 3
+		playerPosX = playerPosX + 1
 	end
 	
 	if Keys.held.Up then
-		direction = 4
-		y = y - 1
+		playerDirection = 4
+		playerPosY = playerPosY - 1
 	end
 end
