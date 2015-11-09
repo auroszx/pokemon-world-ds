7e88ef5ae0b2f4e83d7386d509ed5f3fc5fcf5d0
auroszx <auros.andres@gmail.com>
2015-11-09 11:30:56 -0430

    Extra comment

--- mapScroll.lua
+++ mapScroll.lua
@@ -4,5 +4,8 @@
 -- Library used to scroll loaded maps.
 -- It makes more sense to scroll the map instead of moving the player.
 
+-- Por ahora, los mapas no son tiles individuales, sino imagenes o fondos completos.
+-- For now, maps are not made by drawing tiles per se, but rather full images.
+
 mapPosX = 0
 mapPosY = 0
